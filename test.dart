08fac47b4d66fc0e235034ace9c847f5c69ee035
Iskander <iskander2323>
2023-06-08 18:47:40 +0600

    Second commit

--- test.dart
+++ test.dart
@@ -1,55 +1,37 @@
 import 'dart:io';
 
- numberLong11(String x){
- var response = '+7' + '-(' + x.substring(1,4) + ')-' + x.substring(4,7) + '-' + x.substring(7,9) + '-' +x.substring(9,11);
-return response;
-}
- numberLong10(String x){
- var response = '+7' + '-(' + x.substring(0,3) + ')-' + x.substring(3,6) + '-' + x.substring(6,8) + '-' +x.substring(8,10);
-return response;
-}
-numberLong12(String x){
- var response = '+7' + '-(' + x.substring(2,5) + ')-' + x.substring(5,8) + '-' + x.substring(8,10) + '-' +x.substring(10,12);
-return response;
-}
+// Переделать на одну функцию и дать правильное название
 
+numberToSample(String smth) {
+  var num = smth.substring(smth.length - 10);
+  var response = '+7' +
+      '-(' +
+      num.substring(0, 3) +
+      ')-' +
+      num.substring(3, 6) +
+      '-' +
+      num.substring(6, 8) +
+      '-' +
+      num.substring(8, 10);
+  return response;
+}
 
 void main() {
   print('Enter your number: ');
-  var x = stdin.readLineSync() ?? '0';
-  var first = x[0];
+  var x = stdin.readLineSync() ?? '0'; // Добавить валидацию и трим
+  String pattern = r'^(?:[+0][1-9])?[0-9]{10,12}$';
+  RegExp regExp = new RegExp(pattern);
 
- //print(x.substring(2, 5));
- //77059943878
- //+77059943878
- //87059943878
- //7059943878 
-  if(x.length >= 10){
-    
-  switch(first){
-   case '8': 
-        var answer = numberLong11(x);
-        print(answer);
-        break;
-   case '7': 
-        if(x.length == 11){
-        var answer = numberLong11(x);
-        print(answer);
-        } else if(x.length ==10){
-        var answer = numberLong10(x);
-        print(answer);
-        }
-        break;
-    case '+':
-        var answer = numberLong12(x);
-        print(answer);
-        break;
-    default:
-    print('Uncorrect number for KZ/RU region');
-  }
-  } else  {
-   print('The number is not long enough!');
-  }
+  String smth = x.trim().replaceAll(' ', '').replaceAll('-', '');
+  // Переписать механизм с учётом длины, а не первой цифры
 
-  
+  if (x.isEmpty) {
+    print('Phone number is valid!');
+  } else if (regExp.hasMatch(smth) && x.length >= 10) {
+    //print('Enter the correct number');
+    var response = numberToSample(smth);
+    print(response);
+  } else {
+    print('The wrong number!');
+  }
 }
